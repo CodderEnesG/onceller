8ab73f42d7de05f0cbe0922d803d16b5f0eddbe8
Enes Güngör <enesgungor.gs@gmail.com>
2025-06-04 13:28:26 +0300

    d6

--- project/vite.config.js
+++ project/vite.config.js
@@ -2,13 +2,5 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-  base: '/onceller/',
   plugins: [react()],
-  build: {
-    rollupOptions: {
-      input: {
-        main: 'index.html'
-      }
-    }
-  }
 });
