e9be3c49a798cd91fc82c2a1b200daae6d707187
Enes Güngör <enesgungor.gs@gmail.com>
2025-06-04 15:05:56 +0300

    d9

--- project/vite.config.js
+++ project/vite.config.js
@@ -3,6 +3,6 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-  base: "/", // vercel'de root domain olduğu için bu şekilde olmalı
+  base: "/",
   plugins: [react()],
 });
